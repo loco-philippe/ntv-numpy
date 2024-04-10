e06c5f86eb45469dbf18af6c0efdf154f200d662
Philippe THOMY <philippe@loco-labs.io>
2024-04-10 22:35:23 +0200

    v0.1.1

--- docs/release.rst
+++ docs/release.rst
@@ -1,7 +1,7 @@
 Version x.y.z
 =============
 
-0.1.0 alpha 1 (2024-04-10)
+0.1.1 alpha 1 (2024-04-10)
 --------------------
 - First version
 - exchanging via JSON format
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="ntv_numpy",
-    version="0.1.0",
+    version="0.1.1",
     description="NTV-NumPy : A multidimensional semantic, compact and reversible format for interoperability",
     long_description=long_description,
     long_description_content_type="text/markdown",
